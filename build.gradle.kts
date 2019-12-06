e7b3b35e0c46ab3aabdd8e365f4725724ee404ef
Falko Schumann <falko.schumann@muspellheim.de>
2019-12-06 22:22:09 +0100

    Simplified

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,9 +6,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.3.41"
-    id("com.diffplug.gradle.spotless") version "3.26.1"
     application
+    kotlin("jvm") version "1.3.41"
+    id("com.diffplug.gradle.spotless") version "3.26.1"
 }
 
 repositories {
@@ -16,8 +16,7 @@ repositories {
 }
 
 dependencies {
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation(kotlin("stdlib-jdk8"))
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.5.2")
 }
