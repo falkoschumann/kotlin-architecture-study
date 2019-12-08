7530cc1db171ccc542f7b4f624b111deed3ff910
Falko Schumann <falko.schumann@muspellheim.de>
2019-12-08 11:53:27 +0100

    Added acceptance tests for MVVM

--- mvvm/src/main/kotlin/de/muspellheim/counter/App.kt
+++ mvvm/src/main/kotlin/de/muspellheim/counter/App.kt
@@ -14,7 +14,7 @@ import javafx.stage.Stage
 /** The app builds and binds the components and services. */
 class App : Application() {
 
-    private lateinit var counterService: CounterService
+    internal lateinit var counterService: CounterService
 
     override fun init() {
         counterService = CounterService()
@@ -23,8 +23,8 @@ class App : Application() {
     override fun start(primaryStage: Stage) {
         val loader = FXMLLoader(javaClass.getResource("/views/CounterView.fxml"))
         val root = loader.load<Parent>()
-        val controller = loader.getController<CounterViewController>()
-        controller.injectCounterService(CounterService())
+        val counterViewController = loader.getController<CounterViewController>()
+        counterViewController.injectCounterService(CounterService())
 
         primaryStage.scene = Scene(root)
         primaryStage.title = "Counter - MVVM"
